ec9f00c86e18cae51889dcc4956e7de5e877ba43
Andrew <andrewor1994@outlook.ie>
2019-07-24 18:47:00 +0100

    removed proxy needed for live server

--- app.js
+++ app.js
@@ -11,8 +11,8 @@ window.addEventListener("load", () => {
     navigator.geolocation.getCurrentPosition(position => {
       long = position.coords.longitude;
       lat = position.coords.latitude;
-      const proxy = "https://cors-anywhere.herokuapp.com/";
-      const api = `${proxy}https://api.darksky.net/forecast/1e484c7203fc448b6a95528d449d3971/${lat},${long}`;
+
+      const api = `https://api.darksky.net/forecast/1e484c7203fc448b6a95528d449d3971/${lat},${long}`;
       fetch(api)
         .then(response => {
           return response.json();
